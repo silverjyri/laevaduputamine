ff3ea35dce19cc25350a4e6912412609234e1721
andres.traks@gmail.com <andres.traks@gmail.com@68ca5969-75a9-ca32-7be4-f8fe36011bd8>
2012-03-16 17:03:24 +0000

    Snap ships to grid.

--- trunk/laevadepommitamine/war/Placement.js
+++ trunk/laevadepommitamine/war/Placement.js
@@ -30,19 +30,42 @@ Placement.prototype.render = function() {
 	menu.append(this.chartBtn.render());
 	menu.append(this.historyBtn.render());
 	
+	var onDragMove = function(e) {
+		var data = e.data;
+		var field = data.field;
+		if (e.pageX >= field.left &&
+			e.pageY >= field.top &&
+			e.pageX <= field.right &&
+			e.pageY <= field.bottom) {
+			// Snap to grid
+			var x = parseInt((e.pageX - field.left) / 33);
+			var y = parseInt((e.pageY - field.top) / 33);
+			var pos = $('#ship_container').position();
+			data.clone.dragTo(field.left - pos.left + x * 33, field.top - pos.top + y * 33);
+		} else {
+			data.clone.dragBy(e.pageX - data.x, e.pageY - data.y);
+		}
+	};
+	
 	var onDrag = function(e, ship) {
 		e.preventDefault();
 		var clone = ship.clone();
 		this.clone = clone;
-		var data = {clone: clone, x: e.screenX, y: e.screenY};
+		var field = $('.field');
+		var pl = parseInt(field.css('padding-left')) + 1;
+		var pt = parseInt(field.css('padding-top')) + 1;
+		var pos = field.offset();
+		var field = {left: pos.left + pl, top: pos.top + pt,
+			right: pos.left + field.width(), bottom: pos.top + field.height()};
+		console.log(field);
+		var data = {clone: clone, x: e.pageX, y: e.pageY, field: field};
 		this.shipContainer.append(clone.render());
-		$(document).mousemove(data, function(e) {
-			e.data.clone.dragBy(e.screenX - data.x, e.screenY - data.y);
-		});
+		$(document).mousemove(data, onDragMove);
 	};
 	
 	var onDrop = function(e, ship) {
 		e.preventDefault();
+		$(document).off('mousemove', onDragMove);
 		this.clone.el.remove();
 		delete this.clone;
 	};
@@ -50,14 +73,14 @@ Placement.prototype.render = function() {
 	this.ships = {};
 	var s = 10;
 	this.ships['4h'] = new ShipFloating(4, false, {left: 3*s, onDrag: onDrag, onDrop: onDrop, scope: this});
-	this.ships['3h'] = new ShipFloating(3, false, {left: 2*s, top: 32+s});
-	this.ships['2h'] = new ShipFloating(2, false, {left: s, top: 64+2*s});
-	this.ships['1h'] = new ShipFloating(1, false, {top: 96+3*s});
+	this.ships['3h'] = new ShipFloating(3, false, {left: 2*s, top: 32+s, onDrag: onDrag, onDrop: onDrop, scope: this});
+	this.ships['2h'] = new ShipFloating(2, false, {left: s, top: 64+2*s, onDrag: onDrag, onDrop: onDrop, scope: this});
+	this.ships['1h'] = new ShipFloating(1, false, {top: 96+3*s, onDrag: onDrag, onDrop: onDrop, scope: this});
 	
-	this.ships['4v'] = new ShipFloating(4, true, {top: 32, left: 128+3*s});
-	this.ships['3v'] = new ShipFloating(3, true, {top: 64+s, left: 96+2*s});
-	this.ships['2v'] = new ShipFloating(2, true, {top: 96+2*s, left: 64+s});
-	this.ships['1v'] = new ShipFloating(1, true, {top: 128+3*s, left: 32});
+	this.ships['4v'] = new ShipFloating(4, true, {top: 32, left: 128+3*s, onDrag: onDrag, onDrop: onDrop, scope: this});
+	this.ships['3v'] = new ShipFloating(3, true, {top: 64+s, left: 96+2*s, onDrag: onDrag, onDrop: onDrop, scope: this});
+	this.ships['2v'] = new ShipFloating(2, true, {top: 96+2*s, left: 64+s, onDrag: onDrag, onDrop: onDrop, scope: this});
+	this.ships['1v'] = new ShipFloating(1, true, {top: 128+3*s, left: 32, onDrag: onDrag, onDrop: onDrop, scope: this});
 
 	this.shipCounts = {};
 	this.shipCounts[1] = $('<div class="ship_count" style="left:' + (128+3*s) + 'px;">1</div>');
