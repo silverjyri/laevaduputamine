d4f0e5746900097dacf412b7132efa1adf5e62cf
andres.traks@gmail.com <andres.traks@gmail.com@68ca5969-75a9-ca32-7be4-f8fe36011bd8>
2012-03-16 14:51:11 +0000

    Make a table where to grab ships from.

--- laevadepommitamine/war/ShipFloating.js
+++ laevadepommitamine/war/ShipFloating.js
@@ -1,12 +1,33 @@
 function ShipFloating(length, vertical, options) {
 	if (options) {
+		this.left = options.left;
+		this.top = options.top;
+		this.onDrag = options.onDrag;
+		this.onDrop = options.onDrop;
+		this.scope = options.scope;
 	}
 	this.length = length;
 	this.vertical = vertical;
+	this.left = this.left || 0;
+	this.top = this.top || 0;
 }
 
 ShipFloating.prototype = {
+onMouseUp: function(e) {
+	$.proxy(this.onDrop, this.scope)(e, e.data);
+	$(document).off('mouseup');
+},
+onMouseDown: function(e) {
+	$.proxy(this.onDrag, this.scope)(e, e.data);
+
+	if (this.onDrop) {
+		$(document).mouseup(this, $.proxy(this.onMouseUp, this));
+	}
+},
 onRender: function() {
+	if (this.onDrag) {
+		this.el.mousedown(this, $.proxy(this.onMouseDown, this));
+	}
 },
 render: function() {
 	if (this.el) {
@@ -14,9 +35,17 @@ render: function() {
 	}
 	
 	var el = $('<div class="ship_floating"></div>');
+	if (this.left) {
+		el.css('left', this.left);
+	}
+	if (this.top) {
+		el.css('top', this.top);
+	}
+	el.css('width', this.length * 32);
+	el.css('height', this.top * 32);
 	
 	if (this.length == 1) {
-		el.append('<div class="ship_single"><div>');
+		el.append('<div class="box_floating ship_single"><div>');
 	} else {
 		if (this.vertical) {
 			var i;
@@ -50,5 +79,11 @@ render: function() {
 	this.el = el;
 	this.onRender();
 	return el;
+},
+clone: function() {
+	return new ShipFloating(this.length, this.vertical, {left: this.left, top: this.top});
+},
+dragBy: function(x, y) {
+	this.el.css({left:(x + this.left), top:(y + this.top)});
 }
 };
