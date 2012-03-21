f06a34fed1af8f5eeea4f6d30a17eb153c3674da
andres.traks@gmail.com <andres.traks@gmail.com@68ca5969-75a9-ca32-7be4-f8fe36011bd8>
2012-03-21 01:12:38 +0000

    Choose random player at start.

--- laevadepommitamine/war/Game.js
+++ laevadepommitamine/war/Game.js
@@ -1,7 +1,7 @@
 function Game(ships) {
 	this.player1 = new LocalPlayer("blabla", ships);
 	this.player2 = new AIPlayer();
-	this.currentPlayer = this.player1;
+	this.currentPlayer = Client.rand(1) ? this.player1 : this.player2;
 }
 Game.prototype = new Screen();
 Game.constructor = Game;
@@ -52,37 +52,41 @@ Game.prototype.render = function() {
 					this.currentPlayer = this.player2;
 					this.field1.setStatus('');
 					field.setStatus('Ootan vastase k&auml;iku...');
-
-					var scope = this;
-					var enemyMove = function() {
-						var bombCoords = scope.player2.makeMove();
-						var hit = scope.player1.checkHit(bombCoords);
-						scope.player2.moveResult(bombCoords, hit);
-						scope.field1.addBomb({x: bombCoords.x, y: bombCoords.y, hit: hit});
-						if (hit) {
-							var fullHit = Field.checkFullHit(scope.player1.ships, scope.field1.bombs, bombCoords);
-							if (fullHit) {
-								scope.field1.setShipSunk(fullHit);
-							}
-							setTimeout(enemyMove, 800);
-							return;
-						}
-						scope.field1.setStatus('Sinu kord!');
-						scope.field2.setStatus('');
-						scope.currentPlayer = scope.player1;
-					};
-					
-					setTimeout(enemyMove, 400);
+					setTimeout($.proxy(this.enemyMove, this), 400);
 				}
 			}
 		}
 	}
 	
-	this.field1 = new FieldView({id: '1', onMouseDown: onMouseDown, scope: this, ships: this.player1.ships, status: "Sinu kord!"});
+	var p1status = (this.currentPlayer === this.player1) ? "Sinu kord!" : '';
+	var p2status = (this.currentPlayer === this.player2) ? "Ootan vastase k&auml;iku..." : '';
+	this.field1 = new FieldView({id: '1', onMouseDown: onMouseDown, scope: this, ships: this.player1.ships, status: p1status});
 	this.field2 = new FieldView({id: '2', onMouseDown: onMouseDown, scope: this});
 	el.append(this.field1.render());
 	el.append(this.field2.render());
 
+	if (this.currentPlayer === this.player2) {
+		setTimeout($.proxy(this.enemyMove, this), 400);
+	}
+	
 	this.el = el;
 	return el;
 };
+
+Game.prototype.enemyMove = function() {
+	var bombCoords = this.player2.makeMove();
+	var hit = this.player1.checkHit(bombCoords);
+	this.player2.moveResult(bombCoords, hit);
+	this.field1.addBomb({x: bombCoords.x, y: bombCoords.y, hit: hit});
+	if (hit) {
+		var fullHit = Field.checkFullHit(this.player1.ships, this.field1.bombs, bombCoords);
+		if (fullHit) {
+			this.field1.setShipSunk(fullHit);
+		}
+		setTimeout($.proxy(this.enemyMove, this), 800);
+		return;
+	}
+	this.field1.setStatus('Sinu kord!');
+	this.field2.setStatus('');
+	this.currentPlayer = this.player1;
+}
