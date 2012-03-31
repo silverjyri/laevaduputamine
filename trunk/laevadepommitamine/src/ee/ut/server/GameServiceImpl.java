6e17c4631214362d33f8f74b3e3146a9b40c85a7
andres.traks@gmail.com <andres.traks@gmail.com@68ca5969-75a9-ca32-7be4-f8fe36011bd8>
2012-03-31 22:41:26 +0000

    Database cleanup.

--- trunk/laevadepommitamine/src/ee/ut/server/GameServiceImpl.java
+++ trunk/laevadepommitamine/src/ee/ut/server/GameServiceImpl.java
@@ -7,32 +7,12 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hsqldb.jdbc.JDBCDriver;
-
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import ee.ut.client.GameService;
 
 public class GameServiceImpl extends RemoteServiceServlet implements GameService {
 	private static final long serialVersionUID = 1L;
-
-	public static final String DRIVER = "org.hsqldb.jdbc.JDBCDriver";
-	public static JDBCDriver instance = null;
-	public static final String PROTOCOL = "jdbc:hsqldb:mem:myDb";
-	
-	public static void ensureDatabase() {
-		if (instance == null) {
-			try {
-				instance = (JDBCDriver) Class.forName(DRIVER).newInstance();
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-	}
 	
 	@Override
 	public void createGame() {
@@ -42,7 +22,7 @@ public class GameServiceImpl extends RemoteServiceServlet implements GameService
 			conn = Database.getConnection();
 
 			Statement sta = conn.createStatement();
-			sta.executeUpdate("INSERT INTO Games VALUES (3, 'Mari ootab...')");
+			sta.executeUpdate("INSERT INTO Games (name) VALUES ('Mari ootab...')");
 			sta.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -58,13 +38,6 @@ public class GameServiceImpl extends RemoteServiceServlet implements GameService
 		try {
 			conn = Database.getConnection();
 			Statement sta = conn.createStatement();
-			if (!Database.tableExists(conn, "Games")) {
-				sta.executeUpdate("CREATE TABLE Games ("
-						+ " ID INTEGER PRIMARY KEY,"
-						+ "Name char(50))");
-				sta.executeUpdate("INSERT INTO Games VALUES (1, 'Andres vs. P&auml;tris')");
-				sta.executeUpdate("INSERT INTO Games VALUES (2, 'Silver vs. AI')");
-			}
 			ResultSet resultSet = sta.executeQuery("SELECT Name FROM Games");
 			while (resultSet.next()) {
 				String name = resultSet.getString(1);
