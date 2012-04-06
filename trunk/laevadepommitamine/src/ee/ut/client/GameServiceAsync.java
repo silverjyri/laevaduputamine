ee19dccdc9405bbe35e0fd27d6e1e991e258eeaf
andres.traks@gmail.com <andres.traks@gmail.com@68ca5969-75a9-ca32-7be4-f8fe36011bd8>
2012-04-06 22:47:41 +0000

    Multiplayer works now.

--- trunk/laevadepommitamine/src/ee/ut/client/GameServiceAsync.java
+++ trunk/laevadepommitamine/src/ee/ut/client/GameServiceAsync.java
@@ -10,6 +10,8 @@ public interface GameServiceAsync {
 	public void getGamePlayers(int gameId, AsyncCallback<String[]> callback);
 	public void getUniquePlayerName(AsyncCallback<String> callback);
 	public void joinGame(int gameId, String playerName, AsyncCallback<Void> callback);
+	public void isOpponentReady(int gameId, boolean isOpponent, AsyncCallback<Boolean> callback);
+	public void isOpponentMoveReady(int gameId, boolean isOpponent, AsyncCallback<Boolean> callback);
 	public void playerMove(int gameId, boolean isOpponent, int x, int y, AsyncCallback<Boolean> callback);
 	public void remoteMove(int gameId, boolean isOpponent, AsyncCallback<int[]> callback);
 	public void startGame(int gameId, String playerType, String fieldEnc, AsyncCallback<Boolean> callback);
